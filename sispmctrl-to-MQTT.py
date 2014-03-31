f4997b30ed6a9f485d478edf1d60d82e8297b594
Anton04 <anton.gustafsson@tii.se>
2014-03-31 21:14:29 +0200

    Update sispmctrl-to-MQTT.py

--- sispmctrl-to-MQTT.py
+++ sispmctrl-to-MQTT.py
@@ -1,20 +1,20 @@
-from RFXtrx.pyserial import PySerialTransport
-from RFXtrx import LightingDevice
+import sispm
 import mosquitto,sys
 import json
 import thread
 import time
-from RFXtrx.lowlevel import Lighting2 
 
+#The MQTT format for setting outlets are PREFIX/DEVICE_ID/OUTLET/set 
 
 PREFIX = "sispmcltr"
 MQTT_HOST = "localhost"
+POLL_TIME = 5 #For checking state changes on outlets. 
 
 def on_connect(mosq, rc,a):
     mosq.subscribe(PREFIX+"/#", 0)
 
 def on_message(a,mosq, msg):
-    global transport
+    global devices
     #try:
     if True:
     	print("RECIEVED MQTT MESSAGE: "+msg.topic + " " + str(msg.payload))
@@ -22,10 +22,13 @@ def on_message(a,mosq, msg):
     	name = topics[-2]
     	if topics[-1] == "set":
     	    value = int(msg.payload)
-    	    #print "Set command"
-
-    #Here the control code will go...	    
-    
+    	    if not topics[-3] in devices:
+    	        return
+    	    
+    	    devices[topics[-3]].set_outlet_enabled(int(topic[-2]),bool(value))
+    	    
+    #except:
+    #   print "Error occured while processing command"
     return 
     
 def ControlLoop():
@@ -35,8 +38,13 @@ def ControlLoop():
         client.loop()
         time.sleep(0.1)
 
-transport = PySerialTransport(PORT, debug=True)
-#transport.reset()
+
+#Get all devices.
+
+
+    
+
+
 client = mosquitto.Mosquitto("RFXcom-to-MQTT-client")
 
 
@@ -46,14 +54,35 @@ client.publish("system/sispmctrl-to-MQTT", "Online",1)
 client.on_connect = on_connect
 client.on_message = on_message
 
+#Init
+
+devices = {}
+states = {}
+
 #Start tread...
 thread.start_new_thread(ControlLoop,())
 
 while True:
    #Check if anything changed
    #Send update if it did. 
-   time.sleep(2)
-   
+    
+    #Detect devices
+    n = sispm.get_num_devices()
+    
+    for f in range (0,n):
+        dev=sispm.Sispm(f)
+        filename = dev.device.filename
+        
+        if not filename in devices:
+            devices[filename] = dev
+            states[filename] = [None,None,None,None]
+            #publish
+    
+    #Detect states changes on outlets
+    
+    
+    time.sleep(POLL_TIME)    
+       
     
     
 client.disconnect() 
